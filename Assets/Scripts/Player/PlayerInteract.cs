75acc46f98078f229900389519778f1749af89be
Nguyễn Gia Bảo <capricos2212@gmail.com>
2025-12-11 22:41:39 +0700

    Create script PlayerInteract

--- Assets/Scripts/Player/PlayerInteract.cs
+++ Assets/Scripts/Player/PlayerInteract.cs
@@ -5,9 +5,10 @@ public class PlayerInteract : MonoBehaviour
     [SerializeField] private Camera playerCamera;
     [SerializeField] private LayerMask interactableLayer;
     [SerializeField] private float distanceRaycast = 5f;
+    [SerializeField] private Vector3 offsetRaycast;
 
-    [SerializeField] private ObjectOutline baseObjectOutline;
     [SerializeField] private ObjectOutline currentObjectOutline;
+    [SerializeField] private ObjectOutline baseObjectOutline;
 
     [Header("Show Raycast Settings")]
     [SerializeField] private bool IsShowDebugRay = true;
@@ -17,6 +18,7 @@ public class PlayerInteract : MonoBehaviour
     private void Start()
     {
         playerCamera = Camera.main;
+        offsetRaycast = new Vector3(Screen.width / 2, Screen.height / 2, 0);
     }
 
     private void Update()
@@ -26,7 +28,7 @@ public class PlayerInteract : MonoBehaviour
 
     private void ShootRaycast()
     {
-        Ray ray = playerCamera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));
+        Ray ray = playerCamera.ScreenPointToRay(offsetRaycast);
 
         if (IsShowDebugRay)
         {
@@ -35,29 +37,19 @@ public class PlayerInteract : MonoBehaviour
 
         if (Physics.Raycast(ray, out RaycastHit hitInfo, distanceRaycast, interactableLayer))
         {
-            if (currentObjectOutline != null && baseObjectOutline == null)
-            {
-                baseObjectOutline = currentObjectOutline;
-                currentObjectOutline = hitInfo.collider.GetComponent<ObjectOutline>();
-                if (currentObjectOutline != null)
-                {
-                    currentObjectOutline.SetLayerOutline();
-                    baseObjectOutline.SetLayerDefault();
-                    baseObjectOutline = null;
-                    // debug
-                }
-
-            }
-            if (baseObjectOutline == null && currentObjectOutline == null)
-            {
-                currentObjectOutline = hitInfo.collider.GetComponent<ObjectOutline>();
-
-                if (currentObjectOutline != null)
-                {
-                    currentObjectOutline.SetLayerOutline();
-                }
-            }
-
+            currentObjectOutline = hitInfo.collider.GetComponent<ObjectOutline>();
+        }
+        
+        if (currentObjectOutline != null && baseObjectOutline ==null)
+        {
+            currentObjectOutline.SetLayerOutline();
+            baseObjectOutline = currentObjectOutline;
+        }
+        if(currentObjectOutline != baseObjectOutline)
+        {
+            currentObjectOutline.SetLayerOutline();
+            baseObjectOutline.SetLayerDefault();
+            baseObjectOutline = null;
         }
     }
 }
